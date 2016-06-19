1fbac8b0b11f756a78b890074ab6ec44efee6072
Kathy <katherine.a.cuevas@gmail.com>
2016-06-19 13:59:28 -0400

    phoebe's copy

--- story.rb
+++ story.rb
@@ -14,4 +14,18 @@ require 'pry'
 
 # store the input in a variable
 
+def display_story(user_title, user_rating)
+	puts("You've rate the movie #{title} #{rating}")
+end
+
+puts("Welcome to Movie Rating")
+
+puts("Please enter a movie title")
+
+user_title = gets.strip
+
+puts("How would you rate this movie? (out of ten)")
+
+user_rating = gets.strip
+
 display_story(user_title, user_rating)
